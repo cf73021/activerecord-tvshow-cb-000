e08f1688eb50d28f1ca9d6e5f6ff00057fae19a8
Chris Franklin <cf73021@gmail.com>
2018-10-18 19:49:38 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,2 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.1]
+  def change
+    create_table :shows do |s|
+      s.string :name
+      s.string :network
+      s.string :day
+      s.rating :integer
+    end
+  end
 end
